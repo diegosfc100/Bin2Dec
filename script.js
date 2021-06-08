81ef62be83bd651e0013e63ece0a9eb8fdcd8ba6
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-08 19:09:13 -0300

    add return, wait and error functions

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ const verifyInput = () => {
   } else {
     showError();
   }
+     }
+  
+  // textContent é usado quando se quer alterar o conteúdo de um elemento do DOM.
+const showResult = () => {
+  result.textContent = `${parseInt(input.value, 2)}`; //conversão de binário para decimal
+  waitText.textContent = "Valor convertido para Decimal:";//texto que será exibido.
+  waitText.style.color = "green"; //cor do texto que será exibido.
+  waitText.style.animationDuration = "0s";// valor 0 = sem animação;
+
+}
+
+const showWaiting = () => {
+  waitText.textContent = "Esperando por um número Binário..."; //texto exibido caso usuário não escreva nada.
+  waitText.style.color = "white"; //cor do texto
+  result.textContent = ""; //caso usuário não escreva nada, campo de resultado permanece vazio ""
+
+}
+
+const showError = () => {  
+  waitText.textContent ="Você inseriu um dígito não binário (insira apenas 0 ou 1)";
+  waitText.style.color = "red";
+  result.textContent = "";
+  waitText.style.animationDuration = "0s";
+
+}
