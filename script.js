6c0903a3051825667433cb9a7c13312a47485805
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-08 19:06:53 -0300

    return elements and add regex

---
+++ script.js
@@ -0,0 +1,4 @@
+const input = document.querySelector(".input"); // retornando elemento input
+const waitText = document.querySelector(".waitText"); 
+const result = document.querySelector(".result"); 
+const regex = /^[0-1]+$/; // Regex aceitando sómente digitos 0 e 1.
