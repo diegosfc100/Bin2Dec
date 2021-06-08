7ffb22f3f2acdb84ea9900592dbf9422d46dbc99
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-08 18:14:20 -0300

    add eventListener and arrow Function input

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ const input = document.querySelector(".input"); // retornando elemento input
 const waitText = document.querySelector(".waitText"); 
 const result = document.querySelector(".result"); 
 const regex = /^[0-1]+$/; // Regex aceitando sómente digitos 0 e 1.
+
+input.addEventListener("input", (event) => {
+  verifyInput();
+});
+
+input.addEventListener;
+
+const verifyInput = () => {
+  if (input.value.match(regex)) {
+    showResult();
+  } else if (input.value === "") {
+    showWaiting();
+  } else {
+    showError();
+  }
+}
